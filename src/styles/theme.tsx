7cddf0437d39c3994b51cd7cfe609639cfd2e47d
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-09-09 00:56:02 +0900

    초기 세팅 설정

---
+++ src/styles/theme.tsx
@@ -0,0 +1,23 @@
+import type { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+    color: {
+        primary: '#ff1ef6',
+        bg: '#2d353f',
+        text: '#ffffff',
+    },
+    fonts: {
+        body: 'DM Sans, sans-serif',
+        heading: 'Clear Sans, sans-serif',
+    },
+    fontSizes: {
+        small: '12px',
+        medium: '16px',
+        large: '24px',
+    },
+    fontWeight: {
+        light: 300,
+        regular: 400,
+        bold: 700,
+    },
+};
